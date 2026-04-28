5b5fe480839de16ffb232f6c09706a3764e28a91
MouhammadGaouas <gaouasmouhammad@gmail.com>
2026-04-28 19:08:00 +0100

    adding some stuf

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,11 +1,31 @@
 "use client"
 import React from 'react'
+import { authClient } from '@/lib/auth-client'
+import { useRouter } from 'next/navigation';
 
 export default function SignUp() {
 
+    const { data, error } = await authClient.signUp.email({
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        image, // User image URL (optional)
+    }, {
+        onRequest: (ctx) => {
+            //show loading
+        },
+        onSuccess: (ctx) => {
+            //redirect to the dashboard or sign in page
+        },
+        onError: (ctx) => {
+            // display the error message
+            alert(ctx.error.message);
+        },
+    });
+
     return (
         <div>
-            <h1>SignUp</h1>
+            <h1 className='text-white text-3xl'>SignUp</h1>
         </div>
     )
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+
 // If your Prisma file is located elsewhere, you can change the path
 import { Prisma } from "@/generated/prisma/client";
 
@@ -10,4 +12,5 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [nextCookies()], // make sure this is the last plugin in the array
 });
